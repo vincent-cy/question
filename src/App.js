ff5a5b6eb9924fd532cfa06bc6f388df123c4991
yi.chen <yi.chen@shareworks.cn>
2020-06-20 18:30:51 +0800

    create develop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import DATA from './data.json'
 import axios from 'axios'
-import { Button, Radio, Cascader,Spin, Checkbox, Form, Input, Result, message, Row } from 'antd';
+import {
+  Button, Radio, Cascader, Spin, Layout,
+  Checkbox, Form, Input, Col, Result, message, Row
+} from 'antd';
+const baseApi = process.env.NODE_ENV === "development" ? '/api' : 'http://101.200.182.153:3000/api'
 
 const radioStyle = {
   display: 'block',
@@ -17,15 +21,15 @@ function QuestionCard(props) {
     setLoading(true)
     axios({
       method: 'post',
-      url: 'http://http://101.200.182.153:3000/api/a/answer',
+      url: `${baseApi}/a/answer`,
       data: {
         ...props.useInfo,
         answer: values
       }
-    }).then(res=>{
+    }).then(res => {
       setLoading(false)
       setStep(2)
-    }).catch(err=>{
+    }).catch(err => {
       setLoading(false)
       message.error('提交错误，请重新提交');
     })
@@ -57,9 +61,9 @@ function QuestionCard(props) {
             type = '(单选题)'
             break;
         }
-        const title = `${index + 1}. ${type}${item.question}`
+        const title = `${index + 1}. ${type}${item.question} ${item.answer.toString()}`
         return <Form.Item
-          name={`answer${index+1}`}
+          name={`answer${index + 1}`}
           key={item.question}
           label={title}
           rules={[{ required: true, message: `请完成${index + 1}的答案。` }]}
@@ -99,41 +103,52 @@ function BaseInfoCard(props) {
     setStep(1)
     setUserInfo({ ...values, address: values.address.toString() })
   };
-  return <Form
-    size="large"
-    onFinish={onFinish}
-    wrapperCol={{
-      span: 24,
-    }}
-    layout="vertical"
-  >
-    <div>基本信息</div>
-    <Form.Item label="姓名"
-      rules={[{ required: true, message: '请输入您的姓名！' }]} name="name">
-      <Input />
-    </Form.Item>
+  return <Layout>
+    <img alt='banner' width='100%' src={require('./slog.jpeg')} />
+    <Form
+      size="large"
+      onFinish={onFinish}
+      wrapperCol={{
+        span: 24,
+      }}
+      layout="vertical"
+    >
+      <div>基本信息</div>
+      <Form.Item label="姓名"
+        rules={[{ required: true, message: '请输入您的姓名！' }]} name="name">
+        <Input />
+      </Form.Item>
 
-    <Form.Item label="所在乡镇、社区" rules={[{ required: true, message: '请选择您的单位或地区！' }]} name="address">
-      <Cascader options={DATA.organization} />
-    </Form.Item>
+      <Form.Item label="所在乡镇、社区" rules={[{ required: true, message: '请选择您的单位或地区！' }]} name="address">
+        <Cascader options={DATA.organization} />
+      </Form.Item>
 
-    <Form.Item label="手机号码:" rules={[{ required: true, message: '请输入您的手机号码！' }]} name="phoneNumber">
-      <Input />
-    </Form.Item>
+      <Form.Item label="手机号码:" rules={[{
+        required: true,
+        pattern: /^1[3|4|5|7|8][0-9]\d{8}$/,
+        message: '请输入正确的手机号码！'
+      }]} name="phoneNumber">
+        <Input />
+      </Form.Item>
 
 
-    <Form.Item>
-      <Button type="primary" htmlType="submit"  >
-        开始答题
+      <Form.Item>
+        <Button type="primary" htmlType="submit"  >
+          开始答题
       </Button>
-    </Form.Item>
-  </Form>
+      </Form.Item>
+    </Form>
+  </Layout>
 }
 
 function App() {
   // const [showAnswer, setShow] = useState(false)
   const [useInfo, setUserInfo] = useState({})
-  const [step, setStep] = useState(0)
+  const [step, setStep1] = useState(0)
+  const setStep = e => {
+    window.scrollTo(0, 0);
+    setStep1(e)
+  }
   let Component = BaseInfoCard
   switch (step) {
     case 0:
@@ -143,7 +158,7 @@ function App() {
       Component = QuestionCard
       break
     case 2:
-      Component = Result.bind(null,{status:'success',title:"Successfully Purchased Cloud Server ECS!"})
+      Component = Result.bind(null, { status: 'success', title: "提交数据成功！" })
 
       break
     default:
@@ -152,7 +167,12 @@ function App() {
   }
   return (
     <div className="App">
-      <Component setUserInfo={setUserInfo} useInfo={useInfo} setStep={setStep} />
+      <Row>
+        <Col flex={4}></Col>
+        <Component setUserInfo={setUserInfo} useInfo={useInfo} setStep={setStep} />
+        <Col flex={4}></Col>
+      </Row>
+
     </div>
   );
 }
