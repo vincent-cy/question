d9ccbfb8808dea58d07ee5e63a920f1ea41782e8
yi.chen <yi.chen@shareworks.cn>
2020-06-20 18:27:29 +0800

    Updates

---
+++ precache-manifest.c388a12eea09fdce00444abf98b352be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b48f41a0018435f670cfe622bd43bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3401b2ff226fa891e3bc",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "63626069d247ca8ca778",
+    "url": "/static/css/main.53c000b7.chunk.css"
+  },
+  {
+    "revision": "3401b2ff226fa891e3bc",
+    "url": "/static/js/2.f95d4b2a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f95d4b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63626069d247ca8ca778",
+    "url": "/static/js/main.346b1b11.chunk.js"
+  },
+  {
+    "revision": "f090217cc5a4d2f8e4ca",
+    "url": "/static/js/runtime-main.493a0b80.js"
+  },
+  {
+    "revision": "fc43e17443df4ceeea321af3a7b7912f",
+    "url": "/static/media/slog.fc43e174.jpeg"
+  }
+]);
